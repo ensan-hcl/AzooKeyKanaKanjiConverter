9ecc4f5e5a0e019fca5dfad9db3eca30b02f53b6
ensan <ensan.hcl.dev@gmail.com>
2023-07-23 00:59:45 +0900

    Relax package availablity

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let swiftSettings: [SwiftSetting] = [
 ]
 let package = Package(
     name: "AzooKeyKanakanjiConverter",
-    platforms: [.iOS(.v14), .macOS(.v11)],
+    platforms: [.iOS(.v13), .macOS(.v10_15)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
