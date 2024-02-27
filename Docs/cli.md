f30bfcf2988e596b0ecb192b0e9d31d83cb1054a
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2024-02-27 23:44:01 +0900

    Introduce anco cli tool

---
+++ Docs/cli.md
@@ -0,0 +1,25 @@
+#  anco (azooKey Cli)
+
+`anco`コマンドにより、AzooKeyKanaKanjiConverterをCliで利用することができます。`anco`はデバッグ用ツールの位置付けです。
+
+`anco`を利用するには、最初にinstallが必要です。
+
+```
+sh install_cli.sh
+```
+
+例えば以下のように利用できます。
+
+```
+your@pc Desktop % anco にほんごにゅうりょく --disable_prediction -n 10
+日本語入力
+にほんご入力
+2本ご入力
+2本後入力
+2本語入力
+日本語
+2本
+日本
+にほんご
+2本後
+```
---
+++ Sources/CliTool/Subcommands/RunCommand.swift
@@ -0,0 +1,50 @@
+import KanaKanjiConverterModuleWithDefaultDictionary
+import ArgumentParser
+import Foundation
+
+extension Subcommands {
+    struct Run: ParsableCommand {
+        @Argument(help: "ひらがなで表記された入力")
+        var input: String = ""
+
+        @Option(name: [.customLong("config_n_best")], help: "The parameter n (n best parameter) for internal viterbi search.")
+        var configNBest: Int = 10
+        @Option(name: [.customShort("n"), .customLong("top_n")], help: "Display top n candidates.")
+        var displayTopN: Int = 1
+
+        @Flag(name: [.customLong("disable_prediction")], help: "Disable producing prediction candidates.")
+        var disablePrediction = false
+
+        static var configuration = CommandConfiguration(commandName: "run", abstract: "Show help for this utility.")
+
+        @MainActor mutating func run() {
+            let converter = KanaKanjiConverter()
+            var composingText = ComposingText()
+            composingText.insertAtCursorPosition(input, inputStyle: .direct)
+            let result = converter.requestCandidates(composingText, options: requestOptions())
+            for candidate in result.mainResults.prefix(self.displayTopN) {
+                print(candidate.text)
+            }
+        }
+
+        func requestOptions() -> ConvertRequestOptions {
+            .withDefaultDictionary(
+                N_best: configNBest,
+                requireJapanesePrediction: !disablePrediction,
+                requireEnglishPrediction: false,
+                keyboardLanguage: .ja_JP,
+                typographyLetterCandidate: false,
+                unicodeCandidate: true,
+                englishCandidateInRoman2KanaInput: true,
+                fullWidthRomanCandidate: false,
+                halfWidthKanaCandidate: false,
+                learningType: .nothing,
+                maxMemoryCount: 0,
+                shouldResetMemory: false,
+                memoryDirectoryURL: URL(fileURLWithPath: ""),
+                sharedContainerURL: URL(fileURLWithPath: ""),
+                metadata: .init(appVersionString: "anco")
+            )
+        }
+    }
+}
