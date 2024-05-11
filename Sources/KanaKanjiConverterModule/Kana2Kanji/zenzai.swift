e78a8fc99891dceea88d11de3f2025d241901964
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2024-05-11 22:39:41 +0900

    fix bug

--- Sources/KanaKanjiConverterModule/Kana2Kanji/zenzai.swift
+++ Sources/KanaKanjiConverterModule/Kana2Kanji/zenzai.swift
@@ -3,16 +3,41 @@ import SwiftUtils
 
 extension Kana2Kanji {
     struct ZenzaiCache: Sendable {
-        init(constraint: String) {
+        init(_ inputData: ComposingText, constraint: String, satisfyingCandidate: Candidate?) {
+            self.inputData = inputData
             self.prefixConstraint = constraint
+            self.satisfyingCandidate = satisfyingCandidate
         }
         
-        var prefixConstraint: String
+        private var prefixConstraint: String
+        private var satisfyingCandidate: Candidate?
+        private var inputData: ComposingText
+
+        func getNewConstraint(for newInputData: ComposingText) -> String {
+            let (del, add) = newInputData.differenceSuffix(to: self.inputData)
+            if del == 0 {
+                return self.prefixConstraint
+            } else {
+                if let satisfyingCandidate {
+                    var current = newInputData.convertTarget[...]
+                    var constraint = ""
+                    for item in satisfyingCandidate.data {
+                        if current.hasPrefix(item.ruby) {
+                            constraint += item.ruby
+                            current = current.dropFirst(item.ruby.count)
+                        }
+                    }
+                    return constraint
+                } else {
+                    return ""
+                }
+            }
+        }
     }
 
     /// zenzaiシステムによる完全変換。
     @MainActor func all_zenzai(_ inputData: ComposingText, zenz: Zenz, zenzaiCache: ZenzaiCache?) -> (result: LatticeNode, nodes: Nodes, cache: ZenzaiCache) {
-        var constraint = zenzaiCache?.prefixConstraint ?? ""
+        var constraint = zenzaiCache?.getNewConstraint(for: inputData) ?? ""
         let eosNode = LatticeNode.EOSNode
         var nodes: Kana2Kanji.Nodes = []
         while true {
@@ -22,15 +47,11 @@ extension Kana2Kanji {
                 nodes = draftResult.nodes
             }
             let clauseResult = draftResult.result.getCandidateData()
-            if clauseResult.isEmpty {
-                print("clauseResult was empty!")
-                return (eosNode, nodes, ZenzaiCache(constraint: constraint))
-            }
             let sums: [Candidate] = clauseResult.map {self.processClauseCandidate($0)}
             if sums.isEmpty {
                 print("sums was empty!")
                 // Emptyの場合
-                return (eosNode, nodes, ZenzaiCache(constraint: constraint))
+                return (eosNode, nodes, ZenzaiCache(inputData, constraint: constraint, satisfyingCandidate: nil))
             }
             // resultsを更新
             eosNode.prevs.insert(draftResult.result.prevs[0], at: 0)
@@ -39,16 +60,16 @@ extension Kana2Kanji {
             case .error:
                 // 何らかのエラーが発生
                 print("error")
-                return (eosNode, nodes, ZenzaiCache(constraint: constraint))
+                return (eosNode, nodes, ZenzaiCache(inputData, constraint: constraint, satisfyingCandidate: nil))
             case .pass(let score):
                 // 合格
                 print("passed:", score)
-                return (eosNode, nodes, ZenzaiCache(constraint: constraint))
+                return (eosNode, nodes, ZenzaiCache(inputData, constraint: constraint, satisfyingCandidate: sums[0]))
             case .fixRequired(let prefixConstraint):
                 // 同じ制約が2回連続で出てきたら諦める
                 if constraint == prefixConstraint {
                     print("same constraint:", prefixConstraint)
-                    return (eosNode, nodes, ZenzaiCache(constraint: constraint))
+                    return (eosNode, nodes, ZenzaiCache(inputData, constraint: constraint, satisfyingCandidate: nil))
                 }
                 // 制約が得られたので、更新する
                 print("update constraint:", prefixConstraint)
