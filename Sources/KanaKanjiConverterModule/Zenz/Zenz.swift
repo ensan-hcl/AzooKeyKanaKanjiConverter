875e7270302ac3c979db5deb2aa53f4e6a3ea314
fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2024-08-23 21:12:51 +0900

    small fix for zenzai on windows

--- Sources/KanaKanjiConverterModule/Zenz/Zenz.swift
+++ Sources/KanaKanjiConverterModule/Zenz/Zenz.swift
@@ -14,6 +14,9 @@ import SwiftUtils
                 // this is not percent-encoded
                 self.zenzContext = try ZenzContext.createContext(path: resourceURL.path)
             }
+            #elseif canImport(WinSDK)
+            // remove first "/" from path (for windows)
+            self.zenzContext = try ZenzContext.createContext(path: String(resourceURL.path.dropFirst()))
             #else
             // this is not percent-encoded
             self.zenzContext = try ZenzContext.createContext(path: resourceURL.path)
