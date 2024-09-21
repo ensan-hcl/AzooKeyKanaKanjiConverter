6854c5b022ab3c5e26b971cf7ad335ed97b647a2
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2024-09-22 00:26:08 +0900

    fix: add build command for xcode 16+

--- install_cli.sh
+++ install_cli.sh
@@ -3,4 +3,7 @@ cp -f .build/release/CliTool /usr/local/bin/anco
 
 # FIXME: Unfortunately, in order to use zenzai in anco, you will need to build CliTool with xcodebuild
 #        It is highly desirable to make it work only with `swift build`
+# For Xcode 16 or later
+# xcodebuild -scheme CliTool -destination "platform=macOS,name=My Mac" -configuration Release 
+# For Xcode 15 or former
 # xcodebuild -scheme CliTool -destination "platform=macOS,name=Any Mac" -configuration Release
