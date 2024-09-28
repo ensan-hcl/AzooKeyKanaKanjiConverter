6d482abdca0cba107f7655a6c9591b04923093a7
Miwa / Ensan <ensan.hcl.dev@gmail.com>
2024-09-28 17:06:41 +0900

    fix: build errors in CliTools

--- Sources/CliTool/Anco.swift
+++ Sources/CliTool/Anco.swift
@@ -3,7 +3,7 @@ import ArgumentParser
 
 @main
 public struct Anco: AsyncParsableCommand {
-    public static var configuration = CommandConfiguration(
+    public static let configuration = CommandConfiguration(
         abstract: "Anco is A(zooKey) Kana-Ka(n)ji (co)nverter",
         subcommands: [Subcommands.Run.self, Subcommands.Dict.self, Subcommands.Evaluate.self, Subcommands.Session.self, Subcommands.ExperimentalPredict.self],
         defaultSubcommand: Subcommands.Run.self
